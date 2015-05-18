42c5785f03fc1fc40d4c71dc7bbb1334da22638a
Tom Petit <thopet@uw.edu>
2015-05-18 00:08:36 -0700

    better output for server case 3

--- udp.cpp
+++ udp.cpp
@@ -122,6 +122,7 @@ int clientSlidingWindow( UdpSocket &sock, const int max, int message[], int wind
 
 void serverEarlyRetrans( UdpSocket &sock, const int max, int message[], 
           int windowSize ) {
+    cerr << "server early retrans test:" << endl;
     cerr << "start window size = " << windowSize << endl;
     // init packets array all to false;
     bool* packets = new bool[max];
